3ce02dd438be1b245b843336ca87a72d33c765c3
DESKTOP-LH4O20N\PC <kircanski.bojan@gmail.com>
2024-05-20 12:20:03 +0200

    initialized wcf

---
+++ ChillOutChat/Service/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost serviceHost = new ServiceHost(typeof(Service));
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:10100/Service";
+
+
+            serviceHost.AddServiceEndpoint(typeof(IService), binding, address);
+
+
+            Console.WriteLine("ServiceHost opened.");
+            serviceHost.Open();
+            Console.ReadLine();
+            serviceHost.Close();
+        }
+    }
+}
